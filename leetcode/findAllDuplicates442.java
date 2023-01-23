81a502466033f12972f2873b198c4b31ea6dd576
Sujant Kumar Kv <sujant@Sujants-MacBook-Air.local>
2023-01-23 17:37:47 +0530

    solved leetcode 442

--- leetcode/findAllDuplicates442.java
+++ leetcode/findAllDuplicates442.java
@@ -1,24 +1,35 @@
 package leetcode;
+import java.util.*;
 
 public class findAllDuplicates442 {
     public static void main(String[] args) {
-        
+        int[] arr= {4,3,2,7,8,2,3,1};
+        List<Integer> answer= main(arr);
+        System.out.println(answer);
     }
-    public static int main(int[] arr) {
+    public static List<Integer> main(int[] arr) {
     int i=0, N= arr.length;
+    List<Integer> ans= new ArrayList<>();
     while(i< N) {
-      if(arr[i] != i+1){
             int correctIndex= arr[i] - 1;
             if(arr[i] != arr[correctIndex])
                 swap(arr, i, correctIndex);
             else
-                //arr[i] == arr[correctIndex] i.e. duplicate found
-                return arr[i];
+                i++;
+    }
+
+    for(int j=0; j< N; j++){
+        if(arr[j] != j+1){
+            int correctIndex= arr[j] - 1;
+            if(arr[j] != arr[correctIndex])
+                swap(arr, j, correctIndex);
+            else 
+                ans.add(arr[j]);
         }
         else
-            i++;
+            continue;
     }
-    return -1;
+    return ans;
   }
 
 
